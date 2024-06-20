91d8a39fb836d62fa4e6631933fbd91e15b5f43c
nicoolly <112405868+nickgata@users.noreply.github.com>
2024-06-20 16:55:30 -0300

    20.06

---
+++ script.js
@@ -0,0 +1,4 @@
+const caixaprincipal=document.queryselector("caixa principal");
+const caixaperguntas=document.queryselector("caixa perguntas");
+const caixaAlternativas=document.queryselector("caixa-alternativa");
+const caixaResultado=document.querySelector("caixa-resultado");
